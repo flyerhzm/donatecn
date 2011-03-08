11b462ae60f647dc6624ca5c271ca3a3fb69e169
Richard Huang <flyerhzm@gmail.com>
2011-03-09 00:04:31 +0800

    add a transaction scope histories

--- app/controllers/donates_controller.rb
+++ app/controllers/donates_controller.rb
@@ -1,7 +1,6 @@
 class DonatesController < ApplicationController
   def index
     @donate = Donate.new
-    @transactions = Transaction.where(:trade_status => "TRADE_SUCCESS").order("notify_time desc")
   end
 
   def create
