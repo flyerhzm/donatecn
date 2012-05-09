5809609d51032fdc07dab338a391e62087699071
Richard Huang <flyerhzm@gmail.com>
2012-05-09 08:50:57 +0800

    use new routes in payment engine

--- app/controllers/donates_controller.rb
+++ app/controllers/donates_controller.rb
@@ -11,9 +11,7 @@ class DonatesController < ApplicationController
     if @donate.valid?
       @donate.number = Guid.new.to_s.gsub('-', '')
       if @donate.currency == "usd"
-        #redirect_to_paypal_gateway(:item_name => "donatecn", :amount => @donate.amount)
-        options = {:item_name => "donatecn", :amount => @donate.amount}
-        redirect_to URI.encode("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_ext-enter&redirect_cmd=_xclick&charset=utf-8&business=#{Payment::CONFIGS["paypal"]["email"]}&currenct_code=USD&item_name=#{options[:item_name]}&amount=#{options[:amount]}")
+        redirect_to_paypal_gateway(:item_name => "donatecn", :amount => @donate.amount)
         return
       end
       render :confirm
